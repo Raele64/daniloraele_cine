277ba1c5d5802d225bcc400160aa0ad282357e6b
raele64 <raele64@outlook.com>
2022-09-29 19:16:59 -0300

    Projeto Integrado de Analise e Desenvolvimento de Sistemas

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO `customer` (`b_id`, `email`, `name`, `password`) VALUES (1, 'daniloraele@cine.com.br', 'daniloraele', 'danilo');
+
+INSERT INTO `movies_details` (`movie_id`, `image`, `movie_details`, `movie_name`) VALUES (1, 'https://th.bing.com/th/id/R.3d996fbc6456a11191f3003b21828bd8?rik=JomZwOzGwTve%2fA&pid=ImgRaw&r=0', 'vingadores ultimato 2022', 'vingadores');
+INSERT INTO `movies_details` (`movie_id`, `image`, `movie_details`, `movie_name`) VALUES (2, 'https://tupersonajefavorito.com/wp-content/uploads/2018/05/destacada-1-2.jpg', 'Toy story 4', 'toy story');
+INSERT INTO `movies_details` (`movie_id`, `image`, `movie_details`, `movie_name`) VALUES (3, 'https://www.sonypictures.com.br/sites/brazil/files/2022-03/KEY%20ART_SPIDER%20NO%20WAY%20HOME.JPG', 'Homem Aranha sem volta pra casa', 'homem-aranha');
+INSERT INTO `movies_details` (`movie_id`, `image`, `movie_details`, `movie_name`) VALUES (4, 'https://br.web.img3.acsta.net/c_310_420/pictures/21/04/14/19/06/3385237.jpg', 'velozes e furiosos 9', 'velozes&furiosos9');
+
